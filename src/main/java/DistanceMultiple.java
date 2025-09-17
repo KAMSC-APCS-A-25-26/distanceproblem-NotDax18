b39bdb5c328f90f090a9904576b43e6f75c2b9c9
NotDax18 <dax.cw.robinson@gmail.com>
2025-09-17 14:28:38 -0400

    Update DistanceMultiple.java

--- src/main/java/DistanceMultiple.java
+++ src/main/java/DistanceMultiple.java
@@ -15,7 +15,7 @@
  *  5. Compute the distance between the two points:
  *
  *  6. Print the results
- * 
+ *
  *  7. Repeat until all inputs have been processed.
  *
  * Example Inputs:
@@ -31,9 +31,43 @@ import java.util.Scanner;
 
 
 public class DistanceMultiple {
-    public static void main(String[] args) {
+    public static void main (String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Dax Robinson\nAPCS-A\nLewis 2.8 Distance Program");
+        System.out.println();
 
+        // Prompt for the number of runs
+        System.out.print("Enter number of runs: ");
+        int numRuns = scan.nextInt();
+        System.out.println();
+        scan.nextLine();
 
+        for (int i = 1; i <= numRuns; i++)
+        {
+            // Prompt for (x1,y1)
+            System.out.print("Enter (x1,y1): ");
+            String point = scan.nextLine().trim();
+            point = point.replace("(","").replace(")","");
+            String[] coordinate = point.split(",");
+            double x1 = Double.parseDouble(coordinate[0].trim());
+            double y1 = Double.parseDouble(coordinate[1].trim());
+
+            // Prompt for (x2,y2)
+            System.out.print("Enter (x2,y2): ");
+            String point2 = scan.nextLine().trim();
+            point2 = point2.replace("(","").replace(")","");
+            String[] coordanite2 = point2.split(",");
+            double x2 = Double.parseDouble(coordanite2[0].trim());
+            double y2 = Double.parseDouble(coordanite2[1].trim());
+
+            // Calculate the distance
+            double distance = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+
+            // Print the distance
+            System.out.println("Distance: " + distance);
+            System.out.println();
+        }
     }
 
 }
